23e57c14ceecdb6a98f7f21587f414c2a044e48a
sone <sthaya@gmail.com>
2022-11-28 20:14:29 -0800

    completed cms blog

---
+++ pages/api/comments.ts
@@ -0,0 +1,48 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { GraphQLClient, gql } from "graphql-request";
+
+type Data = {
+  name: string;
+};
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function comments(req, res) {
+  console.log({ graphcmsToken });
+
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${graphcmsToken}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `;
+
+  try {
+    const result = await graphQLClient.request(query, req.body);
+
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
